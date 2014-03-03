6bbf909ed16d7749beb05549f8c36d99b8b8f5b7
Quiet Bunny <quiet_bunny_@outlook.com>
2014-03-03 23:55:13 +0900

    fix bug

--- assets/javascripts/tracker_description.js
+++ assets/javascripts/tracker_description.js
@@ -1,19 +1,21 @@
-$(function(){
-  var target = $("#ticket_tracker_description");
-  var desc = target.find("div").first();
-  var select = $("#issue_tracker_id");
-  target.insertAfter(select);
-  desc.show().slideUp(0);
+var TrackerDescription = {
+  showDescription: function(){
+    var target = $("#ticket_tracker_description");
+    var desc = target.find("div").first();
+    var select = $("#issue_tracker_id");
+    target.insertAfter(select);
+    desc.show().slideUp(0);
 
-  target.find("a").on("click", function(e){
-    if( desc.is(":visible") ){
-      desc.slideUp(0.25);
-    }else{
-      desc.slideDown(0.25);
+    target.find("a").on("click", function(e){
+      if( desc.is(":visible") ){
+        desc.slideUp(0.25);
+      }else{
+        desc.slideDown(0.25);
+      }
+    });
+
+    if(select.find("option:selected").val() != select.find("option[selected]").val()){
+      select.val( select.find("option[selected]").val() );
     }
-  });
-
-  if(select.find("option:selected").val() != select.find("option[selected]").val()){
-    select.val( select.find("option[selected]").val() );
   }
-});
+}
